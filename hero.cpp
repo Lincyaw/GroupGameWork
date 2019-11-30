65bfd72ce350fdbcd39381318b043b2633431201
Lincyaw <53161583+Lincyaw@users.noreply.github.com>
2019-11-30 16:57:50 +0800

    测试commit

---
+++ hero.cpp
@@ -0,0 +1,100 @@
+#include "hero.h"
+
+hero::hero(QObject *parent) : QObject(parent)
+{
+
+}
+void hero::HeroGoLeft()
+{
+    JudgeWhatHeroMeets(Brick);
+    heroPosX-=3;
+
+}
+void hero::HeroGoRight()
+{
+    JudgeWhatHeroMeets(Brick);
+    heroPosX+=3;
+}
+bool hero::HeroJump()
+{
+    JudgeWhatHeroMeets(Brick);
+    if(heroPosY>groundY-HeroJumpHeight-HeroHeight && Jumpflag == 0) //跳跃的上界
+    {
+        heroPosY -= 1;
+        JumpOrNot = 1;
+       // qDebug()<<"向上";
+    }
+    else
+    {
+        Jumpflag = 1;
+        JumpOrNot = 1;
+         //qDebug()<<"到顶了";
+    }
+    if(heroPosY<groundY-HeroHeight && Jumpflag == 1) //跳跃的下界
+    {
+        heroPosY += 1;
+        JumpOrNot = 1;
+        //qDebug()<<"向下";
+    }
+    if(heroPosY>=groundY-HeroHeight && Jumpflag == 1)
+    {
+        JumpOrNot = 0;
+        return 1;
+        //qDebug()<<"到底了";
+
+    }
+    else
+    {
+        JumpOrNot = 1;
+       //qDebug()<<"没到底";
+        return 0;
+    }
+}
+int hero::JudgeWhatHeroMeets(int Type)
+{
+    int BrickLeft = 460,BrickRight = 500,BrickTop = 470;//因为没有写好具体的砖块类, 因此砖块的大小暂时在这里用临时变量
+     int BrickButtom = 480;                                                //代替, 在砖块类写好后可以把这些量换成砖块的posXrange和posYrange
+    switch(Type)
+    {
+        case Brick:
+            if(heroPosX>=BrickLeft && heroPosX<=BrickRight && heroPosY <= BrickButtom)
+            {
+                HeroJumpHeight = groundY-HeroHeight-BrickButtom;
+                qDebug()<<"上面是砖头";
+            }
+            if(heroPosX>=BrickLeft && heroPosX<=BrickRight && heroPosY >= BrickTop)//判断有没有跳到砖头的上面
+            {
+                //这里表示的是跳到了砖块的上方,则修改原来的groundY地平线为BrickTop
+                groundY = BrickTop+HeroHeight;
+                HeroJumpHeight = HeroJumpHeightNormal;
+                qDebug()<<"跳到了上面";
+            }
+            if((heroPosX<BrickLeft || heroPosX>BrickRight) && JumpOrNot==0)
+            {
+                //没有则回到原来的地平线;  注意,如果有其他的地形同理
+                groundY = 540;//MainWindow::GroundY是主窗口设定的地平线
+                HeroJumpHeight = HeroJumpHeightNormal;
+                //flag = 1;
+                StartTimer();
+                qDebug()<<"跳到底了吗?";
+
+            }
+        break;
+    }
+
+
+
+return 0;
+}
+bool hero::HeroFallDown()
+{
+    if(heroPosY<groundY-HeroHeight) //跳跃的下界
+    {
+        heroPosY += 2;
+        return 0;
+    }
+    else
+    {
+       return 1;
+    }
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,106 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPainter>
+#include<QKeyEvent>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+    ui->setupUi(this);
+    this->player = new hero(this);
+
+    resize(WidgetWidth,WidgetHeight);
+
+
+///////////////////////////////人物////////////////////////////////////////////////
+    connect(player,&hero::UpDatePainter,[=](){
+        update();
+    });
+    connect(player,&hero::StartTimer,[=](){
+        FallTimer->start(12);
+    });
+    connect(player,&hero::StopTimer,[=](){
+        FallTimer->stop();
+    });
+    connect(JumpTimer,&QTimer::timeout,[=](){ //跳跃的函数, 降落到地面的时候停止定时器
+        update();
+        if(player->HeroJump())
+        {
+            JumpTimer->stop();
+        }
+    });
+    connect(FallTimer,&QTimer::timeout,[=](){ //跳跃的函数, 降落到地面的时候停止定时器
+        update();
+        if(player->HeroFallDown())
+        {
+            FallTimer->stop();
+        }
+    });
+
+
+
+////////////////////////////////背景///////////////////////////////////////////////
+
+
+
+
+
+
+////////////////////////////////怪物//////////////////////////////////////////////
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+
+        //实例化画家对象  this指定的是绘图设备
+        QPainter painter(this);
+update()
+        //画地面
+        painter.drawLine(QPoint(0,GroundY),QPoint(WidgetWidth,GroundY));
+        //画人
+        painter.drawPixmap(player->heroPosX,player->heroPosY,QPixmap(":/hero/adventurer-run-04.png"));
+}
+void MainWindow::timerEvent(QTimerEvent *ev)
+{
+    if(ev->timerId() == JumpTimer->timerId())
+    {
+        update();
+        player->HeroJump();
+
+    }
+    if(ev->timerId() == FallTimer->timerId())
+    {
+        update();
+        player->HeroFallDown();
+    }
+
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *ev)
+{
+    if(ev->key() == Qt::Key_J ) //跳跃
+    {
+        if(player->JumpOrNot==0)//如果在跳跃的过程中就不进行下面的进程, 即防止二段跳使人物一直在最高度
+        {
+             player->Jumpflag = 0;//把跳跃的方向置为向上
+             JumpTimer->start(12);//每50ms更新一次人物的位置
+        }
+    }
+    if(ev->key() == Qt::Key_A)//左移
+    {
+        player->HeroGoLeft(); //人向左运动
+        update();
+    }
+    if(ev->key() == Qt::Key_D)//右移
+    {
+        player->HeroGoRight();//人向右运动
+        update();
+    }
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
