e26bfa6dad09641f4b5afb99e2db78c923ea611c
ThisisKKya <47145872+ThisisKKya@users.noreply.github.com>
2019-12-03 20:47:10 +0800

    add a dangerous monster
    but it cannot move

---
+++ anemy.cpp
@@ -0,0 +1,14 @@
+#include "anemy.h"
+
+anemy::anemy(QObject *parent, int pos_x,int pos_y) : hero(parent)
+{
+    heroPosX = pos_x;
+    heroPosY = pos_y;
+
+    HeroSkin= QPixmap(":/monster/monster/hell-beast/hell-beast-breath.png");
+    HeroSkin1 = QPixmap(":/hero/adventurer-run-01.png");
+    HeroSkin2 = QPixmap(":/hero/adventurer-run-02.png");
+    HeroSkin3 = QPixmap(":/hero/adventurer-run-03.png");
+    HeroSkin4 = QPixmap(":/hero/adventurer-run-04.png");
+    HeroSkin5 = QPixmap(":/hero/adventurer-run-05.png");
+}
